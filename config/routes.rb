3ecaf4a2fb3718839cfbefc8b5aab32755b28aec
Connor Leech <connorleech@gmail.com>
2013-09-01 14:00:25 +0200

    signup functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 RailsProject::Application.routes.draw do
+  get "users/new"
   get "welcome/index"
 
   root :to => 'welcome#index'
  
   resources :businesses
   resources :admins
+  
+  resources :users
+  #~ root  'static_pages#home'
+  #~ match '/signup',  to: 'users#new',            via: 'get'
 end
