66f819a60bcc42da3ec85e7281e95cb5d563f814
Connor James Leech <connorleech@gmail.com>
2013-11-30 18:08:24 +0300

    passing test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Home page'" do
-      get static_pages_home_path
-      response.status.should be(200)
+      visit static_pages_home_path
       expect(page).to have_content('Home page')
     end
   end
